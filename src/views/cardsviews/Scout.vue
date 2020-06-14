3599529860117de70716d0b3c4ad642ce06ae188
Ivan Pavlovic <pavlovic@vitamin2.ch>
2020-06-14 13:37:37 +0200

    scout page style added

--- src/views/cardsviews/Scout.vue
+++ src/views/cardsviews/Scout.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="router-view">
-    Scout
+  <div class="router-view grid scout-page">
+    <div class="inner-container">
+      <div class="content">
+        <div class="text-cloud">
+          <p>Hi! I am your personal scout. Are you looking for a new player? Just let me know what position you need and I'll be back with my report within 24 hours.</p>
+        </div>
+        <ul class="player-positions">
+          <li>
+            <div class="position-wrapper">GK</div>
+          </li>
+          <li>
+            <div class="position-wrapper">DF</div>
+          </li>
+          <li>
+            <div class="position-wrapper">MF</div>
+          </li>
+          <li>
+            <div class="position-wrapper">FW</div>
+          </li>
+        </ul>
+        <div class="img-wrapper">
+          <SvgIcon icon="scout" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SvgIcon from '@/components/01_atoms/Icons/SvgIcon/index';
+
 export default {
-  name: 'Scout'
+  name: 'Scout',
+  components: { SvgIcon }
 }
 </script>
